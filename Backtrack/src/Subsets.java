b4c6b16c2667bb16e2dd512d8d22e3354f8a7c48
Shashank Hegade <headoftheport1230@gmail.com>
2022-10-08 11:24:29 +0530

    08-10-2022

--- Backtrack/src/Subsets.java
+++ Backtrack/src/Subsets.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 public class Subsets {
 
     public List<List<Integer>> subsets(int[] nums) {
+        /*
+         * backtrack
+         * time: O(n*2^n)
+         * space: O(n)
+         */
         List<List<Integer>> ans = new ArrayList<>();
         helper(nums, 0, ans, new ArrayList<>());
         return ans;
@@ -19,5 +24,50 @@ public class Subsets {
          
          
      }
+
+     public List<List<Integer>> subsets2(int[] nums) {
+        /*
+         * bitmask
+         * time: O(n*2^n)
+         * space: O(n) 
+         */
+        List<List<Integer>> ans = new ArrayList<>();
+        int output  = 1 << nums.length;
+        for(int i = 0; i < output; i++){
+            ans.add(new ArrayList<>());
+        }
+        for(int i = 0; i < output; i++){
+            for(int j = 0; j < nums.length; j++){
+                if( i >> j & 1){
+                    ans[i].add(nums[j]);
+                }
+            }
+        }
+     }
+
+     public List<List<Integer>> subsets3(int[] nums) {
+
+        /*
+         * array expansion
+         * time: O(n*2^n)
+         * space: O(n*2^n)
+         */
+        
+        List<List<Integer>> ans = new ArrayList();
+        ans.add(new ArrayList());
+        for(int i = 0; i < nums.length; i++){
+            List<List<Integer>> temp = new ArrayList();
+            for(List<Integer> item: ans){
+                List<Integer> k = new ArrayList<>(item);
+                k.add(nums[i]);
+                temp.add(k);
+            }
+            
+            ans.addAll(temp);
+        }
+        
+        return ans;
+        
+    }
     
 }
