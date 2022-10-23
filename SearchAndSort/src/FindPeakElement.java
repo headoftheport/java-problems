9728e292629e1aa4d9d6aaa024031cf97d183e4d
Shashank Hegade <headoftheport1230@gmail.com>
2022-10-23 21:53:43 +0530

    23-10-2022

--- SearchAndSort/src/FindPeakElement.java
+++ SearchAndSort/src/FindPeakElement.java
@@ -33,4 +33,24 @@ public class FindPeakElement {
         
     }  
 
+    public int findPeakElement2(int[] nums){
+        int start = 0;
+        int end = nums.length -1;
+
+        while(start <= end){
+            if(start == end){
+                return start;
+            }
+            int mid = (start + end) / 2;
+            if(nums[mid] > nums[mid+1]){
+                end = mid;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+
+        return nums[nums.length-1];
+    }
+
 }
